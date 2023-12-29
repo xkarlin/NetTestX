38541e3274a57fada700d18d1cf3a3dca356367a
Mykyta Kovalenko <nikkv.ottf@gmail.com>
2023-12-29 10:21:56 +0100

    initial project setup with right-click command on .cs file

---
+++ src/NetTestX.VSIX/Commands/GenerateTestsCommand.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading.Tasks;
+using Community.VisualStudio.Toolkit;
+using EnvDTE;
+using EnvDTE80;
+using Microsoft;
+using Microsoft.VisualStudio.Shell;
+
+namespace NetTestX.VSIX;
+
+[Command(PackageIds.GenerateTestsCommand)]
+internal sealed class GenerateTestsCommand : BaseCommand<GenerateTestsCommand>
+{
+    private DTE2 _dte;
+
+    protected override async Task InitializeCompletedAsync()
+    {
+        _dte = await Package.GetServiceAsync(typeof(DTE)) as DTE2;
+        Assumes.Present(_dte);
+    }
+
+    protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
+    {
+        await VS.MessageBox.ShowWarningAsync("NetTestX.VSIX", "Button clicked");
+    }
+
+    protected override void BeforeQueryStatus(EventArgs e)
+    {
+        bool visible = ShouldCommandBeVisible();
+
+        Command.Visible = Command.Enabled = visible;
+    }
+
+    private bool ShouldCommandBeVisible()
+    {
+        ThreadHelper.ThrowIfNotOnUIThread();
+
+        var selectedItems = (UIHierarchyItem[])_dte.ToolWindows.SolutionExplorer.SelectedItems;
+
+        if (selectedItems.Length != 1)
+            return false;
+
+        var projectItem = (ProjectItem)selectedItems[0].Object;
+
+        return projectItem.FileNames[0].EndsWith(".cs");
+    }
+}
---
+++ src/NetTestX.VSIX/NetTestXPackage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using Community.VisualStudio.Toolkit;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Shell;
+
+namespace NetTestX.VSIX;
+
+[PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+[InstalledProductRegistration(Vsix.Name, Vsix.Description, Vsix.Version)]
+[ProvideMenuResource("Menus.ctmenu", 1)]
+[Guid(PackageGuids.NetTestXString)]
+[ProvideAutoLoad(VSConstants.UICONTEXT.SolutionHasSingleProject_string, PackageAutoLoadFlags.BackgroundLoad)]
+[ProvideAutoLoad(VSConstants.UICONTEXT.SolutionHasMultipleProjects_string, PackageAutoLoadFlags.BackgroundLoad)]
+[ProvideAutoLoad(VSConstants.VsEditorFactoryGuid.TextEditor_string, PackageAutoLoadFlags.BackgroundLoad)]
+public sealed class NetTestXPackage : ToolkitPackage
+{
+    protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
+    {
+        await this.RegisterCommandsAsync();
+    }
+}
