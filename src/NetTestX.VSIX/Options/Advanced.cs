bcdcf8f17b658d6058d41ca87073e0f2c7b2da1e
Mykyta Kovalenko <nikkv.ottf@gmail.com>
2024-02-17 17:18:28 +0100

    Confirmation -> Warning

--- src/NetTestX.VSIX/Options/Advanced.cs
+++ src/NetTestX.VSIX/Options/Advanced.cs
@@ -19,7 +19,7 @@ public class Advanced : BaseOptionModel<Advanced>
     public bool IncludeAAAComments { get; set; } = true;
 
     [Category("Advanced")]
-    [DisplayName("Multiple Type Confirmation")]
+    [DisplayName("Multiple Type Warning")]
     [Description("Whether to show the confirmation window when there are multiple types in the file available for generation.")]
     [DefaultValue(true)]
     public bool ShowMultipleTypeWarning { get; set; } = true;
